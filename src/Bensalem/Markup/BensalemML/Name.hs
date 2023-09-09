8ea0c44dc0ffc43b68600108e6fceba2adcea3a2
despresc <christian.j.j.despres@gmail.com>
2023-09-09 16:20:06 -0400

    Move the wired-in names to their own module

--- src/Bensalem/Markup/BensalemML/Name.hs
+++ src/Bensalem/Markup/BensalemML/Name.hs
@@ -12,6 +12,8 @@
 module Bensalem.Markup.BensalemML.Name
   ( -- * Names and built-in resolvers
     Name,
+    NameVariety (..),
+    nameVariety,
     resolveWiredIn,
     resolveByWiredOnly,
   )
@@ -22,11 +24,7 @@ import Bensalem.Markup.BensalemML.Syntax
     SrcName,
     srcNameStr,
   )
-import qualified Data.List as List
-import Data.Map.Strict (Map)
-import qualified Data.Map.Strict as M
-import Data.Text (Text)
-import qualified Data.Text as T
+import Bensalem.Markup.BensalemML.WiredIn
 
 data Name = Name
   { -- | what the name is
@@ -41,73 +39,9 @@ data NameVariety
     NameWiredIn !WiredIn
   deriving (Eq, Ord, Show)
 
--- | A wired-in name. These represent simple and not particularly fundamental
--- markup constructs to start, and will change in future.
-
--- TODO: probably should have this be looser and instead solve this with unique
--- identifiers, but this is fine for now
-
--- For now, these need to match the source names of the entities, aside from the
--- initial WI prefix
-
-data WiredIn
-  = -- | section
-    WIsection
-  | -- | block of TeX math macro definitions
-    WItexmacros
-  | -- | heading of a section
-    WIheading
-  | -- | \"classic\" table
-    WItable
-  | -- | table body
-    WItbody
-  | -- | table row
-    WIrow
-  | -- | ordered list
-    WIol
-  | -- | unordered list
-    WIul
-  | -- | list item
-    WIli
-  | -- | paragraph
-    WIp
-  | -- | emphasized text
-    WIemph
-  | -- | a marker for a physical page
-    WIphysPage
-  | -- | a reference to a place
-    WIplaceRef
-  | -- | a reference to a person
-    WIpersonRef
-  | -- | a reference to a physical place
-    WIlocRef
-  | -- | TeX-style display math
-    WItdmath
-  | -- | TeX-style inline math
-    WItimath
-  | -- | citation of something
-    WIcitation
-  deriving (Eq, Ord, Show, Enum, Bounded)
-
--- | Resolve a 'SrcName' to a wired-in name, if possible
-
--- TODO: this and the temporary resolvers shouldn't live in this module
--- long-term, I would imagine. probably some kind of name resolution module.
--- that would be more feasilbe if we get rid of the 'WiredIn' type like the note
--- on that type says.
-resolveWiredIn :: SrcName -> Maybe Name
-resolveWiredIn nm = case M.lookup (srcNameStr nm) wiredInNameMap of
-  Just nv -> Just $! Name nv nm
-  Nothing -> Nothing
-
-wiredInNameMap :: Map Text NameVariety
-wiredInNameMap = M.fromList $ go <$> [minBound .. maxBound]
-  where
-    go :: WiredIn -> (Text, NameVariety)
-    go w =
-      let !t = T.pack $ List.drop 2 $ show w
-          !n = NameWiredIn w
-       in (t, n)
-
 resolveByWiredOnly :: Node SrcName -> Maybe (Node Name)
-resolveByWiredOnly = traverse resolveWiredIn
+resolveByWiredOnly = traverse go
+  where
+    go sn = case resolveWiredIn $ srcNameStr sn of
+      Just nv -> Just $! Name (NameWiredIn nv) sn
+      Nothing -> Nothing
---
+++ src/Bensalem/Markup/BensalemML/WiredIn.hs
@@ -0,0 +1,93 @@
+-- |
+-- Description : Wired-in name data
+-- Copyright   : 2023 Christian Despres
+-- License     : BSD-2-Clause
+-- Maintainer  : Christian Despres
+module Bensalem.Markup.BensalemML.WiredIn
+  ( WiredIn (..),
+    resolveWiredIn,
+    wiredInArity,
+  )
+where
+
+import qualified Data.List as List
+import Data.Map.Strict (Map)
+import qualified Data.Map.Strict as M
+import Data.Text (Text)
+import qualified Data.Text as T
+
+-- | A wired-in name. These represent simple and not particularly fundamental
+-- markup constructs to start, and will change in future.
+
+-- TODO: probably should have this be looser and instead solve this with unique
+-- identifiers, but this is fine for now
+
+-- For now, these need to match the source names of the entities, aside from the
+-- initial WI prefix
+
+data WiredIn
+  = -- | section
+    WIsection
+  | -- | block of TeX math macro definitions
+    WItexmacros
+  | -- | heading of a section
+    WIheading
+  | -- | \"classic\" table
+    WItable
+  | -- | table body
+    WItbody
+  | -- | table row
+    WIrow
+  | -- | ordered list
+    WIol
+  | -- | unordered list
+    WIul
+  | -- | list item
+    WIli
+  | -- | paragraph
+    WIp
+  | -- | emphasized text
+    WIemph
+  | -- | a marker for a physical page
+    WIphysPage
+  | -- | a reference to a place
+    WIplaceRef
+  | -- | a reference to a person
+    WIpersonRef
+  | -- | a reference to a physical place
+    WIlocRef
+  | -- | TeX-style display math
+    WItdmath
+  | -- | TeX-style inline math
+    WItimath
+  | -- | citation of something
+    WIcitation
+  deriving (Eq, Ord, Show, Enum, Bounded)
+
+-- | the arity of the built-in
+type Arity = Int
+
+-- | The arity of a wired-in type. All the built-in types happen to have arity 1
+-- at the moment.
+
+-- TODO: obviously we should figure this out from a signature/declaration
+-- somewhere and not just have it as a function.
+wiredInArity :: WiredIn -> Arity
+wiredInArity _ = 1
+
+-- | Resolve a 'SrcName' to a wired-in name, if possible
+
+-- TODO: this and the temporary resolvers shouldn't live in this module
+-- long-term, I would imagine. probably some kind of name resolution module.
+-- that would be more feasilbe if we get rid of the 'WiredIn' type like the note
+-- on that type says.
+resolveWiredIn :: Text -> Maybe WiredIn
+resolveWiredIn t = case M.lookup t wiredInNameMap of
+  Just nv -> Just nv
+  Nothing -> Nothing
+
+wiredInNameMap :: Map Text WiredIn
+wiredInNameMap = M.fromList $ go <$> [minBound .. maxBound]
+  where
+    go :: WiredIn -> (Text, WiredIn)
+    go w = (T.pack $ List.drop 2 $ show w, w)
